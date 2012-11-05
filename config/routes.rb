c589f1448bff533350356fc182d2eb7ada24598b
RyanGAllen <alenryan14@gmail.com>
2012-11-05 00:41:56 -0500

    customized some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 HapGc::Application.routes.draw do
   devise_for :users
-
   resources :pins
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
+  get 'board', to: 'pins#index', as: :feed
   root :to => 'pins#index'
+
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'login', to: 'devise/sessions#new', as: :login
+    get 'logout', to: 'devise/sessions#destroy', as: :logout
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
